4b6c1f47d64ba55876ba7afdeb90ce8700274559
triniwiz <fortune.osei@yahoo.com>
2020-09-26 23:13:19 -0400

    fix(canvas): angular support

--- packages/canvas/angular/index.ts
+++ packages/canvas/angular/index.ts
@@ -1,12 +1,10 @@
-import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
-import {registerElement} from '@nativescript/angular';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { registerElement } from '@nativescript/angular';
 
-registerElement('Canvas', () => require('Canvas').Canvas);
-
+registerElement('Canvas', () => require('../').Canvas);
 
 @NgModule({
-  schemas: [NO_ERRORS_SCHEMA]
+	schemas: [NO_ERRORS_SCHEMA],
 })
 // @ts-ignore
-export class CanvasModule {
-}
+export class CanvasModule {}

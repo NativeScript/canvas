34cadecaca1035ab0bbf62f4eabbc6ad5e883a01
triniwiz <fortune.osei@yahoo.com>
2020-09-27 00:22:17 -0400

    fix(pixi): canvas size

--- packages/canvas-pixi/index.ts
+++ packages/canvas-pixi/index.ts
@@ -2,11 +2,13 @@ import '@nativescript/canvas-polyfill';
 import * as Pixii from 'pixi.js';
 
 export class TNSPIXIApplication extends Pixii.Application {
-	constructor({canvas, ...props}) {
+	constructor({ canvas, ...props }) {
 		(global as any).PIXI = (global as any).window.PIXI = (global as any).PIXI || Pixii;
 		super({
 			view: canvas,
-			...props
+			width: canvas.width || undefined,
+			height: canvas.height || undefined,
+			...props,
 		});
 	}
 }

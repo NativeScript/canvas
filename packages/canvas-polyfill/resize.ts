099a84b67128d5bc7bed4594ebd873670bcc0edf
triniwiz <fortune.osei@yahoo.com>
2020-09-27 02:23:49 -0400

    fix(polyfill): resize / rotation

--- packages/canvas-polyfill/resize.ts
+++ packages/canvas-polyfill/resize.ts
@@ -1,5 +1,5 @@
-import {Screen} from "@nativescript/core";
-import {Application} from "@nativescript/core";
+import { Screen } from '@nativescript/core';
+import { Application } from '@nativescript/core';
 /*
  Window Resize Stub
 */
@@ -7,48 +7,28 @@ import {Application} from "@nativescript/core";
 const scale = Screen.mainScreen.scale;
 
 (global as any).window.devicePixelRatio = (global as any).devicePixelRatio = 1;
-(global as any).window.innerWidth = (global as any).innerWidth =
-    Screen.mainScreen.widthPixels;
-(global as any).window.clientWidth = (global as any).clientWidth =
-    Screen.mainScreen.widthPixels;
-(global as any).window.innerHeight = (global as any).innerHeight =
-    Screen.mainScreen.heightPixels;
-(global as any).window.clientHeight = (global as any).clientHeight =
-    Screen.mainScreen.heightPixels;
-(global as any).window.screen = (global as any).screen =
-    (global as any).screen || {};
-(global as any).window.screen.orientation = (global as any).screen.orientation =
-    (global as any).screen.orientation ||
-    (global as any).clientWidth < (global as any).clientHeight
-        ? 0
-        : 90;
+(global as any).window.innerWidth = (global as any).innerWidth = Screen.mainScreen.widthPixels;
+(global as any).window.clientWidth = (global as any).clientWidth = Screen.mainScreen.widthPixels;
+(global as any).window.innerHeight = (global as any).innerHeight = Screen.mainScreen.heightPixels;
+(global as any).window.clientHeight = (global as any).clientHeight = Screen.mainScreen.heightPixels;
+(global as any).window.screen = (global as any).screen = (global as any).screen || {};
+(global as any).window.screen.orientation = (global as any).screen.orientation = (global as any).screen.orientation || (global as any).clientWidth < (global as any).clientHeight ? 0 : 90;
 
 if (!(global as any).__TNS_BROWSER_POLYFILL_RESIZE) {
-    (global as any).__TNS_BROWSER_POLYFILL_RESIZE = true;
-    Application.on("orientationChanged", (args) => {
-        let width = 0;
-        let height = 0;
-        switch (args.newValue) {
-            case "portrait":
-                width = Screen.mainScreen.widthPixels;
-                height = Screen.mainScreen.heightPixels;
-                break;
-            default:
-                width = Screen.mainScreen.heightPixels;
-                height = Screen.mainScreen.widthPixels;
-                break;
-        }
+	(global as any).__TNS_BROWSER_POLYFILL_RESIZE = true;
+	Application.on('orientationChanged', (args) => {
+		const width = Screen.mainScreen.widthPixels;
+		const height = Screen.mainScreen.heightPixels;
 
-        (global as any).window.devicePixelRatio = (global as any).devicePixelRatio = scale;
-        (global as any).window.innerWidth = (global as any).innerWidth = width;
-        (global as any).window.clientWidth = (global as any).clientWidth = width;
-        (global as any).window.innerHeight = (global as any).innerHeight = height;
-        (global as any).window.clientHeight = (global as any).clientHeight = height;
-        (global as any).window.orientation = (global as any).orientation =
-            args.newValue === "portrait" ? 0 : 90;
-        (global as any).window.screen.orientation = (global as any).screen.orientation = (global as any).orientation;
-        if ((global as any).emitter && (global as any).emitter.emit) {
-            (global as any).emitter.emit("resize");
-        }
-    });
+		(global as any).window.devicePixelRatio = (global as any).devicePixelRatio = scale;
+		(global as any).window.innerWidth = (global as any).innerWidth = width;
+		(global as any).window.clientWidth = (global as any).clientWidth = width;
+		(global as any).window.innerHeight = (global as any).innerHeight = height;
+		(global as any).window.clientHeight = (global as any).clientHeight = height;
+		(global as any).window.orientation = (global as any).orientation = args.newValue === 'portrait' ? 0 : 90;
+		(global as any).window.screen.orientation = (global as any).screen.orientation = (global as any).orientation;
+		if ((global as any).emitter && (global as any).emitter.emit) {
+			(global as any).emitter.emit('resize');
+		}
+	});
 }

6484f0b32bdcc4f8f2d3d17954fd515e2dc07edf
triniwiz <fortune.osei@yahoo.com>
2020-09-27 02:24:38 -0400

    fix(canvas): angular build

--- tools/scripts/build-finish.ts
+++ tools/scripts/build-finish.ts
@@ -1,7 +1,7 @@
 const ngPackage = require('ng-packagr');
 const path = require('path');
 const fs = require('fs-extra');
-
+const rimraf = require('rimraf');
 const rootDir = path.resolve(path.join(__dirname, '..', '..'));
 const nxConfigPath = path.resolve(path.join(rootDir, 'nx.json'));
 const nxConfig = JSON.parse(fs.readFileSync(nxConfigPath));
@@ -13,14 +13,25 @@ const publish = cmdArgs[1] === 'publish';
 
 console.log(`Building ${npmScope}/${packageName}...${publish ? 'and publishing.' : ''}`);
 
-function finishPreparation() {
+function buildAngular() {
 	fs.copy(path.join('dist', 'out-tsc'), path.join('dist', 'packages', packageName))
 		.then(() => fs.copy(path.join('tools', 'assets', 'publishing'), path.join('dist', 'packages', packageName)))
-		.then(() => console.log(`${npmScope}/${packageName} ready to publish.`))
+		.then(() => {
+			rimraf.sync(path.join('dist', 'out-tsc'));
+			console.log(`${npmScope}/${packageName} ready to publish.`);
+		})
 		.catch((err) => console.error(err));
-	// fs.copy(path.join('tools', 'assets', 'publishing'), path.join('dist', 'packages', packageName))
-	// 	.then(() => console.log(`${npmScope}/${packageName} ready to publish.`))
-	// 	.catch((err) => console.error(err));
 }
 
-finishPreparation();
+function finishPreparation() {
+	fs.copy(path.join('tools', 'assets', 'publishing'), path.join('dist', 'packages', packageName))
+		.then(() => console.log(`${npmScope}/${packageName} ready to publish.`))
+		.catch((err) => console.error(err));
+}
+
+if (fs.existsSync(path.join(rootDir, 'packages', packageName, 'angular'))) {
+	// package has angular specific src, build it first
+	buildAngular();
+} else {
+	finishPreparation();
+}

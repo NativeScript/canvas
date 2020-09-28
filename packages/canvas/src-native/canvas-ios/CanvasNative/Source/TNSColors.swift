c5b79eed7bb4f821361b541db81ada499d2d0d17
triniwiz <fortune.osei@yahoo.com>
2020-09-27 21:19:15 -0400

    fix(canvas-ios): scanHex warning

--- packages/canvas/src-native/canvas-ios/CanvasNative/Source/TNSColors.swift
+++ packages/canvas/src-native/canvas-ios/CanvasNative/Source/TNSColors.swift
@@ -205,14 +205,27 @@ extension UIColor {
                 let fourth = cString[3..<4]
                 cString =  "" + first +  first  + second  + second  + third  + third + fourth + fourth
             }
-            var rgbValue:UInt32 = 0
-            Scanner(string: cString).scanHexInt32(&rgbValue)
-            self.init(
-                red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                alpha: CGFloat(1.0)
-            )
+            
+            if #available(iOS 13.0, *){
+                var rgbValue:UInt64 = 0
+                Scanner(string: cString).scanHexInt64(&rgbValue)
+                self.init(
+                    red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                    green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                    blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                    alpha: CGFloat(1.0)
+                )
+            }else {
+                var rgbValue:UInt32 = 0
+                Scanner(string: cString).scanHexInt32(&rgbValue)
+                self.init(
+                    red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                    green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                    blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                    alpha: CGFloat(1.0)
+                )
+            }
+            
         }else if(string.starts(with: "rgba")){
             let rgba = string.replacingOccurrences(of: "rgba(", with: "").replacingOccurrences(of: ")", with: "").replacingOccurrences(of: " ", with: "").split(separator: ",")
             let formatter = NumberFormatter()

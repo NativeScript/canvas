f2727cbe77bb2e63609b46a0081a2fe0b6092586
triniwiz <fortune.osei@yahoo.com>
2021-04-14 13:47:44 -0400

    feat: initial v8 native module setup

---
+++ packages/canvas/src-native/canvas-native/v8-bindings/wrapper.h
@@ -0,0 +1,8 @@
+//
+// Created by Osei Fortune on 14/04/2021.
+//
+
+#ifndef CANVAS_NATIVE_WRAPPER_H
+#define CANVAS_NATIVE_WRAPPER_H
+#include "vendor/android/include/v8.h"
+#endif //CANVAS_NATIVE_WRAPPER_H

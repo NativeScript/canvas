690ce2a8c11a99f0f0a390755cd3d27a0f62540e
Osei Fortune <fortune.osei@yahoo.com>
2024-02-01 04:10:57 -0400

    fix(ios): layout

--- packages/canvas/Dom/Dom.ts
+++ packages/canvas/Dom/Dom.ts
@@ -1,9 +1,9 @@
-import { EventData, LayoutBase, ViewBase } from '@nativescript/core';
+import { EventData, LayoutBase, ViewBase, Utils, View } from '@nativescript/core';
 import { Canvas } from '../Canvas';
 import { Image } from './Image';
 import { Paint } from './Paint';
 export class Dom extends LayoutBase {
-	_canvas;
+	_canvas: Canvas;
 	_children = [];
 
 	constructor() {
@@ -13,7 +13,14 @@ export class Dom extends LayoutBase {
 	}
 
 	createNativeView(): Object {
-		return new android.widget.LinearLayout(this._context);
+		if (global.isIOS) {
+			return UIView.new();
+		}
+
+		if (global.isAndroid) {
+			return new android.widget.LinearLayout(this._context);
+		}
+		return null;
 	}
 
 	initNativeView(): void {
@@ -21,20 +28,35 @@ export class Dom extends LayoutBase {
 		this._addView(this._canvas);
 	}
 
+	public onLayout(left: number, top: number, right: number, bottom: number): void {
+		super.onLayout(left, top, right, bottom);
+		View.layoutChild(this, this._canvas, left, top, right, bottom);
+	}
+
+	public onMeasure(widthMeasureSpec: number, heightMeasureSpec: number) {
+		const nativeView = this.nativeView;
+		if (nativeView) {
+			const width = Utils.layout.getMeasureSpecSize(widthMeasureSpec);
+			const height = Utils.layout.getMeasureSpecSize(heightMeasureSpec);
+			View.measureChild(this, this._canvas, width, height);
+			this.setMeasuredDimension(width, height);
+		}
+	}
+
 	_ready(args: EventData) {
-		console.time('_ready');
 		for (const child of this._children) {
 			child.draw();
 		}
-
-		// this._children.forEach((child) => {
-		// 	child.draw();
-		// });
-		console.timeEnd('_ready');
 	}
 	_addViewToNativeVisualTree(view: ViewBase, atIndex?: number): boolean {
 		if (view === this._canvas) {
-			this.nativeView.addView(this._canvas.nativeView);
+			if (global.isIOS) {
+				this.nativeView.addSubview(this._canvas.nativeView);
+			}
+
+			if (global.isAndroid) {
+				this.nativeView.addView(this._canvas.nativeView);
+			}
 			return true;
 		} else if (view instanceof Paint || view instanceof Image) {
 			view._addCanvas(this._canvas);

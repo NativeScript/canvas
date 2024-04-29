9813f4ba3d11e6bdb0e2dd94fc6f1eeca11a9d95
Nathan Walker <walkerrunpdx@gmail.com>
2024-04-29 10:27:19 -0700

    feat: visionos

--- packages/canvas/src-native/canvas-ios/build.sh
+++ packages/canvas/src-native/canvas-ios/build.sh
@@ -3,7 +3,8 @@
 echo "Set exit on simple errors"
 set -e
 
-rm -rf $(PWD)/dist
+BUILD_DIR=$(PWD)/dist
+rm -rf $BUILD_DIR
 
 echo "Build for iphonesimulator"
 xcodebuild \
@@ -13,7 +14,7 @@ xcodebuild \
     -destination "generic/platform=iOS Simulator" \
     -configuration Release \
     clean build \
-    BUILD_DIR=$(PWD)/dist \
+    BUILD_DIR=$BUILD_DIR \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES 
 
@@ -25,17 +26,47 @@ xcodebuild \
     -destination "generic/platform=iOS" \
     -configuration Release \
     clean build \
-    BUILD_DIR=$(PWD)/dist \
+    BUILD_DIR=$BUILD_DIR \
     CODE_SIGN_IDENTITY="" \
     CODE_SIGNING_REQUIRED=NO \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES 
 
+# Only available with Xcode >= 15.2
+echo "Build for visionOS Simulator"
+xcodebuild \
+    -project CanvasNative.xcodeproj \
+    -scheme CanvasNative \
+    -configuration Release \
+    -destination "generic/platform=visionOS Simulator" \
+    clean build \
+    BUILD_DIR=$BUILD_DIR \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+    -quiet
+
+echo "Build for visionOS"
+xcodebuild \
+    -project CanvasNative.xcodeproj \
+    -scheme CanvasNative \
+    -configuration Release \
+    -destination "generic/platform=visionOS" \
+    clean build \
+    BUILD_DIR=$BUILD_DIR \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+    -quiet
+
 echo "Creating XCFramework"
+BASE=$BUILD_DIR/Release
 xcodebuild \
     -create-xcframework \
-    -framework $(PWD)/dist/Release-iphoneos/CanvasNative.framework \
-    -debug-symbols $(PWD)/dist/Release-iphoneos/CanvasNative.framework.dSYM \
-    -framework $(PWD)/dist/Release-iphonesimulator/CanvasNative.framework \
-    -debug-symbols $(PWD)/dist/Release-iphonesimulator/CanvasNative.framework.dSYM \
-    -output $(PWD)/dist/CanvasNative.xcframework
+    -framework $BASE-iphoneos/CanvasNative.framework \
+    -debug-symbols $BASE-iphoneos/CanvasNative.framework.dSYM \
+    -framework $BASE-iphonesimulator/CanvasNative.framework \
+    -debug-symbols $BASE-iphonesimulator/CanvasNative.framework.dSYM \
+    -framework     $BASE-xrsimulator/CanvasNative.framework \
+    -debug-symbols $BASE-xrsimulator/CanvasNative.framework.dSYM \
+    -framework     $BASE-xros/CanvasNative.framework \
+    -debug-symbols $BASE-xros/CanvasNative.framework.dSYM \
+    -output $BASE/CanvasNative.xcframework

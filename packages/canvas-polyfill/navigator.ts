2092bdb569c5700b970e912f9b0dd37b9e65413b
Osei Fortune <fortune.osei@yahoo.com>
2024-11-23 06:44:50 -0400

    fix(polyfill): improve userAgent & appVersion

--- packages/canvas-polyfill/navigator.ts
+++ packages/canvas-polyfill/navigator.ts
@@ -4,13 +4,18 @@ import { GPU } from '@nativescript/canvas';
 const gpu = new GPU();
 
 export class Navigator {
+	appCodeName = 'Mozilla';
+	appName = 'Netscape';
 	product = 'NativeScript';
-	userAgent = 'NativeScript';
+	userAgent = `Mozilla/5.0 (${this.platform === 'Android' ? 'Linux' : this.platform}; ${this.platform} ${Device.osVersion};) (KHTML, like Gecko) Mobile Safari/537.36 NativeScript/` + `${global.__runtimeVersion ?? '8.0.0'}`.replaceAll('"', '');
 	vendor = '';
 	vendorSub = '';
-	appVersion = Device.osVersion;
+	appVersion = `5.0 (${this.platform === 'Android' ? 'Linux' : this.platform}; ${this.platform} ${Device.osVersion};) (KHTML, like Gecko) Mobile Safari/537.36 NativeScript/` + `${global.__runtimeVersion ?? '8.0.0'}`.replaceAll('"', '');
 	maxTouchPoints = 5;
 	standalone = true;
+	language() {
+		return Device.language;
+	}
 	get gpu() {
 		return gpu;
 	}

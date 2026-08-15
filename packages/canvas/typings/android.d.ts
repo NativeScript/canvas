638a265c621c5e898fe23f0d5d6c73c4c166144e
Osei Fortune <fortune.osei@yahoo.com>
2026-08-15 02:13:06 -0400

    fix(android): mutiple canvas render issues

--- packages/canvas/typings/android.d.ts
+++ packages/canvas/typings/android.d.ts
@@ -263,6 +263,7 @@ declare module org {
 				public static nativeResizeCustomSurface(param0: number, param1: number, param2: number, param3: number, param4: boolean, param5: number): void;
 				public static getBuffer(key: number): java.nio.ByteBuffer;
 				public getNativeContext(): number;
+				public releaseNativeContext(): void;
 				public getNativeContextString(): string;
 				public getIgnoreTouchEvents(): boolean;
 				public static context2DRender(context: number): void;
